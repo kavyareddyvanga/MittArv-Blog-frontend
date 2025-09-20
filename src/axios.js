e5da302c66a73f95f6d619f7a71ec7917a26906e
kavyareddyvanga <145844235+kavyareddyvanga@users.noreply.github.com>
2025-09-20 23:01:10 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mittarv-backend-qjwr.onrender.com/api",
+  baseURL: "https://backend-mittarv.onrender.com/api",
   withCredentials: true, // send cookies/session if your backend uses them
 });
 
