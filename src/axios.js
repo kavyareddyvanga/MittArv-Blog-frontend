5a7857a458a9ff23e3debe218b5fe08920ae2fee
kavyareddyvanga <145844235+kavyareddyvanga@users.noreply.github.com>
2025-09-21 12:38:53 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-mittarv.onrender.com/api",
+  baseURL: "https://backend-mittarv.onrender.com/api",//If testing locally, change it to something like http://localhost:5000/api. if 5000 is the port where backend is running
   withCredentials: true, // send cookies/session if your backend uses them
 });
 
